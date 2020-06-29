7b9b2231e8ac82157a529e8b16ab14f73272487d
Matt Lacey <matt@mrlacey.co.uk>
2020-06-29 14:46:06 +0100

    Add custom analyzer

---
+++ NoGridAnalyzer/NoGridAnalyzer/MyElementAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RapidXaml;
+
+namespace NoGridAnalyzer
+{
+    public class MyElementAnalyzer : ICustomAnalyzer
+    {
+        public string TargetType() => "Grid";
+
+        public AnalysisActions Analyze(RapidXamlElement element, ExtraAnalysisDetails extraDetails)
+        {
+            return AnalysisActions.HighlightWithoutAction(
+                errorType: RapidXamlErrorType.Error,
+                code: "NOGRD01",
+                description: "Do not use Grids!");
+        }
+    }
+}
